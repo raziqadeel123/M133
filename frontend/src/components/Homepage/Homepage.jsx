07400b41654a6cb35d1355f66ba73d1672e9b57c
raziq adeel <raziq.adeel7@gmail.com>
2022-10-04 16:47:19 +0200

    mini changes

--- frontend/src/components/Homepage/Homepage.jsx
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -1,38 +1,36 @@
-import React from "react";
-import { Button, ButtonSet} from "@carbon/react";
+import React from 'react';
+import { Button, ButtonSet } from '@carbon/react';
 import BannerImage from './Assets/man-sweeping-each-and-every-infected-places.svg';
-import {  Link } from "react-router-dom";
-import { Footer } from "../Footer/Footer";
+import { Link } from 'react-router-dom';
+import { Footer } from '../Footer/Footer';
 
-const Homepage = () =>{
- 
-
-return (
-  <>
-    <div className='cds--grid cds--grid--condensed'>
-      <div className='cds--row'>
-        <div className='cds--col'>
-          <h1 className='slogan--h1'>The best Cleaning App for your Company</h1>
-          <ButtonSet>
-            <Link className='btn--mar-right' to='/login'>
-              <Button kind='primary'>Login</Button>
-            </Link>
-            <Link className='btn--mar-right' to='/register'>
-              <Button kind='secondary'>Registration</Button>
-            </Link>
-          </ButtonSet>
-        </div>
-        <div className='cds--col'>
-          <img
-            className='img--banner'
-            src={BannerImage}
-            alt='Women-cleaning-Homepage-Banner'
-          />
+const Homepage = () => {
+  return (
+    <>
+      <div className='cds--grid cds--grid--condensed'>
+        <div className='cds--row'>
+          <div className='cds--col'>
+            <h1 className='slogan--h1'>You can login and Register yourself</h1>
+            <ButtonSet>
+              <Link className='btn--mar-right' to='/login'>
+                <Button kind='primary'>Login</Button>
+              </Link>
+              <Link className='btn--mar-right' to='/register'>
+                <Button kind='secondary'>Registration</Button>
+              </Link>
+            </ButtonSet>
+          </div>
+          <div className='cds--col'>
+            <img
+              className='img--banner'
+              src={BannerImage}
+              alt='Women-cleaning-Homepage-Banner'
+            />
+          </div>
         </div>
       </div>
-    </div>
-    <Footer></Footer>
-  </>
-);
-}
-export default Homepage
+      <Footer></Footer>
+    </>
+  );
+};
+export default Homepage;
