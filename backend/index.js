1e7c99a4e51cd02ee9d97f8e6f2671fe32cdba3d
raziq adeel <raziq.adeel7@gmail.com>
2022-10-04 16:24:46 +0200

    changes in backend and adding css

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import db from '../backend/config/database.js';
 import router from '../backend/routes/index.js';
+import bodyParser from 'body-parser';
+import session from 'express-session';
 
 dotenv.config();
 const app = express();
@@ -15,9 +17,29 @@ try {
   console.log(error);
 }
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'DELETE'],
+    credentials: true,
+  })
+);
+//app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(cookieParser());
+bodyParser.urlencoded({ extended: true });
 app.use(express.json());
 app.use(router);
+
+app.use(
+  session({
+    key: 'userId',
+    secret: 'tjop82374982ajdsfsaf',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
 
 app.listen(5000, () => console.log('Server running at port 5000'));
